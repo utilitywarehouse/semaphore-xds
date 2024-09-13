c46073ac420f5a56c52d0bc256bb3276b7b2c97b
ffilippopoulos <ffilippopoulos@uw.co.uk>
2024-09-13 21:37:08 +0100

    Change example client to the simplest 2 calls for testing
    Build with grpc-go v1.65.0

--- example/client/main.go
+++ example/client/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
-	"strings"
-	"sync"
 	"time"
 
 	echo "github.com/utilitywarehouse/semaphore-xds/example/client/echo"
@@ -14,38 +11,31 @@ import (
 	_ "google.golang.org/grpc/xds"
 )
 
-var (
-	flagGrpcServerAddresses = flag.String("grpc-server-addresses", "", "Comma separated list of echo server addresses")
-)
-
 func main() {
-	flag.Parse()
-	if *flagGrpcServerAddresses == "" {
-		log.Fatal("Must provide a list grpc server addresses")
+	serverAddr1 := "xds:///grpc-echo-server.labs:50051"
+	serverAddr2 := "xds:///grpc-echo-another-server.labs:50051"
+	conn1, err := grpc.Dial(serverAddr1, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
 	}
-	addresses := strings.Split(*flagGrpcServerAddresses, ",")
-	var wg sync.WaitGroup
-	for _, address := range addresses {
-		wg.Add(1)
-		go func(address string) {
-			log.Println("Looking up service %s", address)
-			conn, err := grpc.Dial(address, grpc.WithInsecure())
-			if err != nil {
-				log.Fatalf("Could not connect %v", err)
-			}
-			defer conn.Close()
-
-			c := echo.NewEchoServerClient(conn)
-			ctx := context.Background()
-			for {
-				r, err := c.SayHello(ctx, &echo.EchoRequest{Name: "unary RPC msg "})
-				if err != nil {
-					log.Printf("Could not get RPC %v\n", err)
-				}
-				log.Printf("RPC Response: %v", r)
-				time.Sleep(1 * time.Second)
-			}
-		}(address)
+	defer conn1.Close()
+	c1 := echo.NewEchoServerClient(conn1)
+	ctx := context.Background()
+	r1, err := c1.SayHello(ctx, &echo.EchoRequest{Name: "unary RPC msg "})
+	if err != nil {
+		log.Printf("Could not get RPC %v\n", err)
 	}
-	wg.Wait()
+	log.Printf("RPC Response: %v", r1)
+	time.Sleep(1 * time.Second)
+	conn2, err := grpc.Dial(serverAddr2, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+	}
+	defer conn1.Close()
+	c2 := echo.NewEchoServerClient(conn2)
+	r2, err := c2.SayHello(ctx, &echo.EchoRequest{Name: "unary RPC msg "})
+	if err != nil {
+		log.Printf("Could not get RPC %v\n", err)
+	}
+	log.Printf("RPC Response: %v", r2)
 }
