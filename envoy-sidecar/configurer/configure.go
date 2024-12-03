9c5d7491700838b12b76d2f723d4fa99a7b676ec
ffilippopoulos <ffilippopoulos@uw.co.uk>
2024-12-03 09:03:21 +0000

    Use port_redirect to route to multiple domains via envoy sidecar

--- envoy-sidecar/configurer/configure.go
+++ envoy-sidecar/configurer/configure.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"path"
 	"strings"
 	"text/template"
 )
 
-type Listener struct {
+const (
+	EnvoyMainListenPort = 18001
+)
+
+type MainListener struct {
+	Port    int
+	Domains []RouteDomain
+}
+
+type RouteDomain struct {
+	Name   string
+	Domain string
+	Port   int
+}
+type TargetListener struct {
+	Name            string
+	Port            int
 	RouteConfigName string
 }
 
@@ -21,19 +38,32 @@ type XdsCluster struct {
 }
 
 type EnvoyConfig struct {
-	NodeID     string
-	ClusterID  string
-	Listeners  string
-	Clusters   string
-	XdsCluster string
+	NodeID       string
+	ClusterID    string
+	MainListener string
+	Listeners    string
+	Clusters     string
+	XdsCluster   string
 }
 
 func makeEnvoyConfig(nodeID, envoySidecarTargets, XdsServerAddress, XdsServerPort string) (string, error) {
-	listeners, clusters := extractConfigFromTargets(envoySidecarTargets)
+	main, listeners, clusters := extractConfigFromTargets(envoySidecarTargets)
+	// Generate main listener config
+	mainListenerTmplPath := "./templates/main-listener.tmpl"
+	mainListenerTmplBase := path.Base(mainListenerTmplPath)
+	tmpl, err := template.New(mainListenerTmplBase).ParseFiles(mainListenerTmplPath)
+	if err != nil {
+		return "", err
+	}
+	var renderedMainListener bytes.Buffer
+	err = tmpl.Execute(&renderedMainListener, main)
+	if err != nil {
+		return "", err
+	}
 	// Generate Listeners Config
-	listenersTmplPath := "./templates/listeners.tmpl"
+	listenersTmplPath := "./templates/target-listeners.tmpl"
 	listenersTmplBase := path.Base(listenersTmplPath)
-	tmpl, err := template.New(listenersTmplBase).ParseFiles(listenersTmplPath)
+	tmpl, err = template.New(listenersTmplBase).ParseFiles(listenersTmplPath)
 	if err != nil {
 		return "", err
 	}
@@ -73,11 +103,12 @@ func makeEnvoyConfig(nodeID, envoySidecarTargets, XdsServerAddress, XdsServerPor
 
 	// Generate the Envoy config
 	envoyConfig := EnvoyConfig{
-		NodeID:     nodeID,
-		ClusterID:  nodeID, // needed by envoy, add node id here as a dummy value here
-		Listeners:  renderedListeners.String(),
-		Clusters:   renderedClusters.String(),
-		XdsCluster: renderedXdsCluster.String(),
+		NodeID:       nodeID,
+		ClusterID:    nodeID, // needed by envoy, add node id here as a dummy value here
+		MainListener: renderedMainListener.String(),
+		Listeners:    renderedListeners.String(),
+		Clusters:     renderedClusters.String(),
+		XdsCluster:   renderedXdsCluster.String(),
 	}
 	envoyConfigTmplPath := "./templates/envoy-config.tmpl"
 	envoyConfigTmplBase := path.Base(envoyConfigTmplPath)
@@ -102,13 +133,30 @@ func makeEnvoyConfig(nodeID, envoySidecarTargets, XdsServerAddress, XdsServerPor
 // and routeConfig names. Clusters names should be derived from the above and
 // follow the form: service.namespace.port to comply with the xds naming
 // limitations and how semaphore-xds configures cluster names.
-func extractConfigFromTargets(envoySidecarTargets string) ([]Listener, []Cluster) {
-	listeners := []Listener{}
+func extractConfigFromTargets(envoySidecarTargets string) (MainListener, []TargetListener, []Cluster) {
+	port := EnvoyMainListenPort
+	main := MainListener{
+		Port: port,
+	}
+	domains := []RouteDomain{}
+	listeners := []TargetListener{}
 	for _, target := range strings.Split(envoySidecarTargets, ",") {
-		listeners = append(listeners, Listener{
+		port++
+		lName := fmt.Sprintf("listener_%d", port)
+		listeners = append(listeners, TargetListener{
+			Name:            lName,
+			Port:            port,
 			RouteConfigName: target,
 		})
+		rName := fmt.Sprintf("route_%d", port)
+		domains = append(domains, RouteDomain{
+			Name:   rName,
+			Domain: target,
+			Port:   port,
+		})
 	}
+	main.Domains = domains
+
 	clusters := []Cluster{}
 	for _, l := range listeners {
 		clusterName := strings.Join(strings.Split(l.RouteConfigName, ":"), ".")
@@ -116,5 +164,5 @@ func extractConfigFromTargets(envoySidecarTargets string) ([]Listener, []Cluster
 			Name: clusterName,
 		})
 	}
-	return listeners, clusters
+	return main, listeners, clusters
 }
