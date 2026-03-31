e4dd8fb43db689d40c193d0f56906b8b3677f157
ffilippopoulos <ffilippopoulos@uw.co.uk>
2026-03-31 13:10:48 +0100

    Fix example server so we can build again

--- example/server/main.go
+++ example/server/main.go
@@ -9,24 +9,13 @@ import (
 
 	echo "github.com/utilitywarehouse/semaphore-xds/example/server/echo"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
-var (
-	grpcport = flag.String("grpcport", ":50051", "grpcport")
-
-	hs *health.Server
-
-	conn *grpc.ClientConn
-)
-
-const ()
+var grpcport = flag.String("grpcport", ":50051", "grpcport")
 
 type server struct {
 }
@@ -51,17 +40,6 @@ func (s *server) SayHelloStream(in *echo.EchoRequest, stream echo.EchoServer_Say
 	return nil
 }
 
-type healthServer struct{}
-
-func (s *healthServer) Check(ctx context.Context, in *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
-	log.Printf("Handling grpc Check request")
-	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
-}
-
-func (s *healthServer) Watch(in *healthpb.HealthCheckRequest, srv healthpb.Health_WatchServer) error {
-	return status.Error(codes.Unimplemented, "Watch is not implemented")
-}
-
 func main() {
 
 	flag.Parse()
@@ -83,7 +61,9 @@ func main() {
 
 	echo.RegisterEchoServerServer(s, &server{})
 
-	healthpb.RegisterHealthServer(s, &healthServer{})
+	hs := health.NewServer()
+	hs.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthpb.RegisterHealthServer(s, hs)
 
 	s.Serve(lis)
 
